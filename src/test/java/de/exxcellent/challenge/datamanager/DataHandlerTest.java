cb7fdbb544c2cc78efaad0e79f424b97fafab72e
Irina <79757191+iriola@users.noreply.github.com>
2021-02-28 18:27:39 +0100

    chore(DataHandlerTest): changed integers to floats to test handling non-integers temperatures

--- src/test/java/de/exxcellent/challenge/datamanager/DataHandlerTest.java
+++ src/test/java/de/exxcellent/challenge/datamanager/DataHandlerTest.java
@@ -12,15 +12,15 @@ public class DataHandlerTest {
 
     @Test
     void testToGetDayWithSmallestTempSpread() {
-        WeatherDataPoint day1 = new WeatherDataPoint("1", 14, 3, 11f);
-        WeatherDataPoint day2 = new WeatherDataPoint("2", 4, 3, 1f);
-        WeatherDataPoint day3 = new WeatherDataPoint("3", 20, 3, 17f);
-        WeatherDataPoint day4 = new WeatherDataPoint("4", 4, 3, 1f);
+        WeatherDataPoint day1 = new WeatherDataPoint("1", 14.9f, 3.3f, 11.6f);
+        WeatherDataPoint day2 = new WeatherDataPoint("2", 4.9f, 3.1f, 1.8f);
+        WeatherDataPoint day3 = new WeatherDataPoint("3", 20, 3, 17);
+        WeatherDataPoint day4 = new WeatherDataPoint("4", 4, 3, 1);
         List<WeatherDataPoint> weatherDataPoints = Arrays.asList(day1,day2,day3, day4);
         DataHandler dataHandler = new DataHandler();
 
         String day = dataHandler.getDayWithSmallestTempSpread(weatherDataPoints);
 
-        assertEquals("2", day, "Incorrect day was returned");
+        assertEquals("4", day, "Incorrect day was returned");
     }
 }
