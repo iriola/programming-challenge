f6b242121aa052a51d58d8c5880f7225199bb576
Irina <79757191+iriola@users.noreply.github.com>
2021-02-28 18:11:49 +0100

    feat(Data handling): added unit test and method to parse list of lists to get list of weather data points

--- src/main/java/de/exxcellent/challenge/datamanager/WeatherDataHandler.java
+++ src/main/java/de/exxcellent/challenge/datamanager/WeatherDataHandler.java
@@ -2,19 +2,71 @@ package de.exxcellent.challenge.datamanager;
 
 import de.exxcellent.challenge.datamodel.WeatherDataPoint;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
+/**
+ * class used to parse list of lists (table-like structure) to get a list of required weather data points.
+ * contains a method to calculate the temperature spread.
+ */
 public class WeatherDataHandler {
 
+    String dayHeaderLabel = "Day";
+    String maxTemperatureHeaderLabel = "MxT";
+    String minTemperatureHeaderLabel = "MnT";
+
+
     /**
-     * method to calculate values of maximum temperature and minimum temperature
+     * method to calculate temperature spread from maximum temperature and minimum temperature
      * @param maxTemp is the maximum temperature
      * @param minTemp is the minimum temperature
      * @return calculated temperature spread based on maxTemp and minTemp
      */
     public float calculateTemperatureSpread(float maxTemp, float minTemp) {
         return maxTemp - minTemp;
+    }
+
+
+
+    /**
+     * method to parse a list of all data points from a list of lists of strings to get a list of weather data points
+     * @param allDataPoints is a list of lists of strings (table-like structure; outer list: rows, inner list: cells)
+     * @return a list of weather data point
+     */
+    public List<WeatherDataPoint> parseDataPoints(List<List<String>> allDataPoints) {
+
+        // HashMap contains the value from the first row (as key) and the index (as value) at which this data can be found in a row
+        // generated based on the first (header row) of the table (mapping defined through the instance variables dayHeaderLabel, maxTemperatureHeaderLabel, minTemperatureHeaderLabel)
+        HashMap<String, Integer> firstRowIndexMap = new HashMap<>();
+        for(String firstRowElement : allDataPoints.get(0)) {
+            if (firstRowElement.contains(dayHeaderLabel)) {
+                  firstRowIndexMap.put(firstRowElement, allDataPoints.get(0).indexOf(firstRowElement));
+            }
+            if(firstRowElement.contains(maxTemperatureHeaderLabel)){
+                firstRowIndexMap.put(firstRowElement, allDataPoints.get(0).indexOf(firstRowElement));
+            }
+            if(firstRowElement.contains(minTemperatureHeaderLabel)){
+                firstRowIndexMap.put(firstRowElement, allDataPoints.get(0).indexOf(firstRowElement));
+            }
+        }
+
+        List<WeatherDataPoint> weatherDataPoints = new ArrayList<>();
+
+        // generate a list of weather data point using the key of the map to get the right cell in the table row
+        for(List<String> wdo : allDataPoints.subList(1, allDataPoints.size())){
+
+            try {
+                String day = wdo.get(firstRowIndexMap.get("Day"));
+                float maxTemp = Float.parseFloat(wdo.get(firstRowIndexMap.get("MxT")));
+                float minTemp = Float.parseFloat(wdo.get(firstRowIndexMap.get("MnT")));
+
+                weatherDataPoints.add(new WeatherDataPoint(day, maxTemp, minTemp, calculateTemperatureSpread(maxTemp, minTemp)));
+
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        return weatherDataPoints;
     }
 
 }
--- src/test/java/de/exxcellent/challenge/datamanager/WeatherDataHandlerTest.java
+++ src/test/java/de/exxcellent/challenge/datamanager/WeatherDataHandlerTest.java
@@ -28,6 +28,43 @@ public class WeatherDataHandlerTest {
         assertEquals(3, tempSpread, "Failed: wrong value");
     }
 
+    @Test
+    void testParseDataPoints() {
+        List<String> firstRow = new ArrayList<>()
+        {{
+            add("Day");
+            add("MxT");
+            add("MnT");
+            add("AvT");
+        }};
+        List<String> secondRow = new ArrayList<>()
+        {{
+            add("1");
+            add("88.8");
+            add("59.3");
+            add("74");
+        }};
+        List<String> thirdRow = new ArrayList<>()
+        {{
+            add("2");
+            add("79.8");
+            add("63.6");
+            add("71");
+        }};
+        List<List<String>> listOfDataPoints = Arrays.asList(firstRow, secondRow, thirdRow);
+
+        List<WeatherDataPoint> weatherDataPoints = weatherDataHandler.parseDataPoints(listOfDataPoints);
+
+
+        WeatherDataPoint day1 = new WeatherDataPoint("1", 88.8f, 59.3f, 29.5f);
+        WeatherDataPoint day2 = new WeatherDataPoint("2", 79.8f, 63.6f, 16.2f);
+
+        List<WeatherDataPoint> expectedList = Arrays.asList(day1,day2);
+
+        assertEquals(expectedList, weatherDataPoints, "wrong return list");
+
+    }
+
 
 
 }
