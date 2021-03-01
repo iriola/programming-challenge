e9edacd2b94cd85a68fef26daa70442d671d74a3
Irina <79757191+iriola@users.noreply.github.com>
2021-03-01 18:37:33 +0100

    refactor(DataHandler) added unit tests and methods to handle football challenge

--- src/main/java/de/exxcellent/challenge/datamanager/DataHandler.java
+++ src/main/java/de/exxcellent/challenge/datamanager/DataHandler.java
@@ -1,6 +1,7 @@
 package de.exxcellent.challenge.datamanager;
 
 import de.exxcellent.challenge.datainput.CSVReader;
+import de.exxcellent.challenge.datamodel.FootballDataPoint;
 import de.exxcellent.challenge.datamodel.WeatherDataPoint;
 
 import java.util.Comparator;
@@ -8,18 +9,20 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 /**
- * interface between main / App class and the logic of weather task
+ * interface between main / App class and the logic of weather task and football task
  */
 public class DataHandler {
 
+    CSVReader reader;
 
     /**
      * method to get the day with smallest temperature spread out of a list with weather data points
      * determines the smallest temperature spread with the help of the Stream API
+     *
      * @param weatherDataPoints a list with weather data points
-     * @return the day as a string
+     * @return the day with the smallest temperature spread
      */
-    public String getDayWithSmallestTempSpread(List<WeatherDataPoint> weatherDataPoints) {
+    public String findDayWithSmallestTempSpread(List<WeatherDataPoint> weatherDataPoints) {
         WeatherDataPoint minTempSpread = weatherDataPoints
                 .stream()
                 .min(Comparator.comparing(WeatherDataPoint::getTemperatureSpreadValue))
@@ -28,18 +31,52 @@ public class DataHandler {
     }
 
     /**
-     * calls a set of methods (readData from CSVReader, parseDataPoints WeatherDataHandler) to get the day with the
+     * calls a set of methods (readData from CSVReader, parseDataPoints from WeatherDataHandler) to get the day with the
      * smallest temperature spread
+     *
      * @param dataPath contains the path to the data which should be read
      * @return the day with smallest temperature spread
      */
     public String getDayWithSmallestTempSpread(String dataPath) {
 
-        CSVReader reader = new CSVReader();
+        reader = new CSVReader();
         List<List<String>> listOfAllElements = reader.readData(dataPath);
 
         WeatherDataHandler weatherDataHandler = new WeatherDataHandler();
         List<WeatherDataPoint> weatherDataPoints = weatherDataHandler.parseDataPoints(listOfAllElements);
-        return getDayWithSmallestTempSpread(weatherDataPoints);
+        return findDayWithSmallestTempSpread(weatherDataPoints);
     }
+
+    /**
+     * calls a set of methods (readData from CSVReader, parseDataPoints from FootballDataHandler) to get the team with
+     * the smallest goal distance
+     *
+     * @param dataPath contains the path to the data which should be read
+     * @return the team with smallest goal distance
+     */
+    public String getTeamWithSmallestGoalDistance(String dataPath) {
+
+        reader = new CSVReader();
+        List<List<String>> listOfAllElements = reader.readData(dataPath);
+
+        FootballDataHandler footballDataHandler = new FootballDataHandler();
+        List<FootballDataPoint> footballDataPoints = footballDataHandler.parseDataPoints(listOfAllElements);
+        return findTeamWithSmallestGoalDistance(footballDataPoints);
+    }
+
+    /**
+     * method to get the team with smallest goal spread out of a list with football data points
+     * determines the smallest goal distance with the help of the Stream API
+     *
+     * @param footballDataPoints a list with football data points
+     * @return the team with the smallest goal distance
+     */
+    public String findTeamWithSmallestGoalDistance(List<FootballDataPoint> footballDataPoints) {
+        FootballDataPoint minDiff = footballDataPoints
+                .stream()
+                .min(Comparator.comparing(FootballDataPoint::getSmallestGoalDistance))
+                .orElseThrow(NoSuchElementException::new);
+        return minDiff.getTeamName();
+    }
+
 }
--- src/test/java/de/exxcellent/challenge/datamanager/DataHandlerTest.java
+++ src/test/java/de/exxcellent/challenge/datamanager/DataHandlerTest.java
@@ -1,6 +1,9 @@
 package de.exxcellent.challenge.datamanager;
 
+import de.exxcellent.challenge.datamodel.FootballDataPoint;
 import de.exxcellent.challenge.datamodel.WeatherDataPoint;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -9,18 +12,57 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DataHandlerTest {
+    DataHandler dataHandler;
 
+    @BeforeEach
+    void setUp() {
+        dataHandler = new DataHandler();
+    }
+
+    @DisplayName("Ensure to determine the correct day with smallest temperature spread")
     @Test
-    void testToGetDayWithSmallestTempSpread() {
+    void testFindDayWithSmallestTempSpread() {
         WeatherDataPoint day1 = new WeatherDataPoint("1", 14.9f, 3.3f, 11.6f);
         WeatherDataPoint day2 = new WeatherDataPoint("2", 4.9f, 3.1f, 1.8f);
         WeatherDataPoint day3 = new WeatherDataPoint("3", 20, 3, 17);
         WeatherDataPoint day4 = new WeatherDataPoint("4", 4, 3, 1);
-        List<WeatherDataPoint> weatherDataPoints = Arrays.asList(day1,day2,day3, day4);
-        DataHandler dataHandler = new DataHandler();
+        List<WeatherDataPoint> weatherDataPoints = Arrays.asList(day1, day2, day3, day4);
 
-        String day = dataHandler.getDayWithSmallestTempSpread(weatherDataPoints);
+
+        String day = dataHandler.findDayWithSmallestTempSpread(weatherDataPoints);
 
         assertEquals("4", day, "Incorrect day was returned");
+    }
+
+    @DisplayName("Ensure to get the right day (lowest temperature spread) using CSV-file")
+    @Test
+    void testGetDayWithSmallestTempSpread() {
+        String filePath = "de/exxcellent/challenge/weather.csv";
+
+        assertEquals("14", dataHandler.getDayWithSmallestTempSpread(filePath));
+    }
+
+    @DisplayName("Ensure to determine the team with smallest goal distance ")
+    @Test
+    void testFindSmallestGoalDistance() {
+        FootballDataPoint team1 = new FootballDataPoint("Arsenal", 79, 36, 43);
+        FootballDataPoint team2 = new FootballDataPoint("Liverpool", 67, 30, 37);
+        FootballDataPoint team3 = new FootballDataPoint("Manchester United", 87, 45, 42);
+        FootballDataPoint team4 = new FootballDataPoint("Newcastle", 74, 52, 22);
+
+        List<FootballDataPoint> footballDataPoints = Arrays.asList(team1, team2, team3, team4);
+
+        assertEquals("Newcastle", dataHandler.findTeamWithSmallestGoalDistance(footballDataPoints)
+                , "Expected team is different to actual team.");
+    }
+
+    @DisplayName("Ensure to get the right team (lowest goal distance) using CSV-file")
+    @Test
+    void testGetSmallestGoalDistance() {
+        String filePath = "de/exxcellent/challenge/football.csv";
+
+        assertEquals("Aston_Villa", dataHandler.getTeamWithSmallestGoalDistance(filePath)
+                , "Expected team is different to actual team.");
+
     }
 }
